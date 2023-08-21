830759ab5cdaad47fa2c369b8a0d36658b84ce95
蒋衡 <jiangheng@guhecloud.com>
2023-08-21 16:48:54 +0800

    增加下拉菜单选择

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -4,34 +4,79 @@ import avator from '@/assets/avatar.jpg'
 import './index.scss'
 import '../../assets/iconfont/iconfont.css'
 import { Input, Avatar } from '@nutui/nutui-react';
-import { Notice } from '@nutui/icons-react';
+import { Notice, ArrowDown2 } from '@nutui/icons-react';
 function Main() {
+    const menu = [
+        {
+            id: 0,
+            name: '首页',
+            path:'/'
 
+        },
+        {
+            id: 1,
+            name: '文章',
+            path:"/article"
 
+        },
+        {
+            id: 2,
+            name: '留言板',
+            path:"/message"
+
+        },
+    ]
+    const [currentMenu, setcurrentMenu] = useState<number>(0)
+    const [openMenu, setopenMenu] = useState<boolean>(false)
+    const openMneu = () => {
+        setopenMenu(!openMenu)
+    }
+    const changemenu = (id: number) => {
+        setcurrentMenu(id)
+        setopenMenu(false)
+
+    }
 
     return (
         <>
-            <div className='w-100% flex justify-around items-center'>
-                <div>首页</div> <div> <Input placeholder="请输入文本" /></div><div className='flex  items-center'>
+            <div className='w-100% flex justify-around items-center position-relative'>
+                <div className='flex items-center cursor-pointer' onClick={openMneu}>
+                    <div className='text-13px'>
+                        {menu[currentMenu].name}
+                    </div>
+                    <ArrowDown2></ArrowDown2>
+                </div>
+                <div> <Input placeholder="请输入文本" /></div>
+                <div className='flex  items-center'>
                     <Notice  ></Notice>
                     <Avatar
-                    className='ml-10px'
+                        className='ml-10px'
                         size="small"
                         src={avator}
                     />
+
+
                 </div>
+                {openMenu ? <div className='position-absolute w-100px h-250px bg-#fff z-999 left-0px top-40px text-center'>
+                    {
+                        menu.map((res: { id: number, name: string }) => {
+                            return <div onClick={() => changemenu(res.id)} key={res.id} className={'text-12px mt-10px color-#515767'} style={{ color: currentMenu == res.id ? '#1e80ff' : '' }}>{res.name}</div>
+
+                        })}
+                </div> : <></>}
+
             </div>
             <div className='h-10px bg-#f2f3f5 w-100%'>
             </div>
-            
-            <div className='userInfo'>
-            <video data-scroll data-scroll-direction data-scroll-speed autoPlay loop muted playsInline className='trm-banner-cover trm-active-el'>
-                        <source src="//cdn.moji.com/websrc/video/autumn20190924.mp4" type="video/mp4; " />
-                    </video>
-        
+
+            <div className='userInfo'  >
+                <video data-scroll data-scroll-direction data-scroll-speed autoPlay loop muted playsInline className='trm-banner-cover trm-active-el'>
+                    <source src="//cdn.moji.com/websrc/video/autumn20190924.mp4" type="video/mp4; " />
+                </video>
+
                 <div className='trm-mc-header'>
                     <div className='trm-avatar' >
-                   
+
                         <img className='avatar' src={avator} alt="" />
                     </div>
                     <h5>衡衡蒋</h5>
